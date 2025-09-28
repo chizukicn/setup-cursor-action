1356c11ff2f29b77bb5372eb3efdb74ccfa09439
chizukicn <chizukicn@outlook.com>
2025-09-28 17:20:38 +0800

    chore: update

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "tsdown";
+import pkg from "./package.json" with { type: "json" };
 
 export default defineConfig({
   dts: false,
   exports: false,
   format: ["cjs"],
   entry: "./src/index.ts",
+  noExternal: Object.keys(pkg.dependencies ?? {})
 });
